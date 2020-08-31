ee19163b10023d2f420922970cdc8ce784c04c3f
wwjdtm <jje0ng@kookmin.ac.kr>
2020-09-01 00:29:14 +0900

    사진첨부기능추가

--- winkproject/winkproject/urls.py
+++ winkproject/winkproject/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 import polioapp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', polioapp.views.home, name='home'),
     path('polioapp/', include('polioapp.urls')),
-]
+    
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
